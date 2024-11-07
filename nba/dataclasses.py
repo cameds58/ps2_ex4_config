9801680a513c9efe07179e0a856abe23f61daf98
ps2016-mt <ps2016@cam.ac.uk>
2024-11-07 23:21:55 +0000

    redone config Ex 4

--- nba/dataclasses.py
+++ nba/dataclasses.py
@@ -1,1 +1,20 @@
-# TODO: define your data classes here
+from pydantic import BaseModel
+from typing import List, Dict
+
+class DataConfig(BaseModel):
+    table: str
+    data_path: str
+    features_columns: List[str]
+
+class ModelParams(BaseModel):
+    max_iter: int
+    random_state: int
+
+class ModelConfig(BaseModel):
+    name: str
+    type: str
+    params: ModelParams
+
+class Config(BaseModel):
+    data: DataConfig
+    model: ModelConfig
